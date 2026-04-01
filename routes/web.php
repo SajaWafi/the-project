04f78b4be6b28ce25ae5097b041aafb60def201c
fatima <fatimadwaya70@gmail.com>
2026-04-02 00:39:02 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome-screen');
 });
+Route::get('/welcome-second', function () {
+    return view('welcome-second');
+})->name('welcome.second');
+Route::get('/login-page', function () {
+    return view('login-page');
+})->name('login.page');
